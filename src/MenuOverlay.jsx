2a303495c66aa00457c5474d8a3d28f125136ac9
Gary February <gfebruary@gmail.com>
2024-06-25 21:52:03 +0200

    sidebar

--- src/MenuOverlay.jsx
+++ src/MenuOverlay.jsx
@@ -1,7 +1,13 @@
 import React, { useState, useEffect } from "react";
+import { slide as Menu } from "react-burger-menu";
 
 const MenuOverlay = () => {
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 660);
   const [showMenu, setShowMenu] = useState(false);
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, []);
 
   useEffect(() => {
     const timerId = setTimeout(() => {
@@ -11,11 +17,23 @@ const MenuOverlay = () => {
     return () => clearTimeout(timerId);
   }, []);
 
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 660);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   if (!showMenu) {
     return null;
   }
 
-  function scrollToSection(sectionId) {
+  const scrollToSection = (sectionId) => {
     if (sectionId === "home") {
       window.scrollTo({ top: 0, behavior: "smooth" });
     } else {
@@ -24,46 +42,82 @@ const MenuOverlay = () => {
         element.scrollIntoView({ behavior: "smooth" });
       }
     }
-  }
-  
+  };
 
   return (
     <div className="menu-overlay">
-      <ul className="menu-list">
-        {/* <li className="menu-item menu-item-left">Home</li> */}
-
-        <li
-          className="menu-item menu-item-left"
-          onClick={() => scrollToSection("home")}>
-          Home
-        </li>
-
-        <li
-          className="menu-item menu-item-center"
-          onClick={() => scrollToSection("about")}>
-          About
-        </li>
-
-        <li
-          className="menu-item menu-item-center"
-          onClick={() => scrollToSection("experience")}>
-          Experience
-        </li>
-
-        <li
-          className="menu-item menu-item-center"
-          onClick={() => scrollToSection("projects")}>
-          Projects
-        </li>
-
-        <li
-          className="menu-item menu-item-center"
-          onClick={() => scrollToSection("contact")}>
-          Contact
-        </li>
-
-        <li className="menu-item menu-item-right">CV</li>
-      </ul>
+      {isSmallScreen ? (
+        <Menu right>
+          <a
+            className="menu-item menu-item-left"
+            onClick={() => scrollToSection("home")}>
+            Home
+          </a>
+          <a
+            className="menu-item menu-item-center"
+            onClick={() => scrollToSection("about")}>
+            About
+          </a>
+          <a
+            className="menu-item menu-item-center"
+            onClick={() => scrollToSection("experience")}>
+            Experience
+          </a>
+          <a
+            className="menu-item menu-item-center"
+            onClick={() => scrollToSection("projects")}>
+            Projects
+          </a>
+          <a
+            className="menu-item menu-item-center"
+            onClick={() => scrollToSection("contact")}>
+            Contact
+          </a>
+          <a
+            className="menu-item menu-item-right"
+            // href="/path-to-cv.pdf"
+            // target="_blank"
+            // rel="noopener noreferrer"
+          >
+            CV
+          </a>
+        </Menu>
+      ) : (
+        <ul className="menu-list">
+          <li
+            className="menu-item menu-item-left"
+            onClick={() => scrollToSection("home")}>
+            Home
+          </li>
+          <li
+            className="menu-item menu-item-center"
+            onClick={() => scrollToSection("about")}>
+            About
+          </li>
+          <li
+            className="menu-item menu-item-center"
+            onClick={() => scrollToSection("experience")}>
+            Experience
+          </li>
+          <li
+            className="menu-item menu-item-center"
+            onClick={() => scrollToSection("projects")}>
+            Projects
+          </li>
+          <li
+            className="menu-item menu-item-center"
+            onClick={() => scrollToSection("contact")}>
+            Contact
+          </li>
+          <li className="menu-item menu-item-right">
+            <a
+            // href="/path-to-cv.pdf" target="_blank" rel="noopener noreferrer"
+            >
+              CV
+            </a>
+          </li>
+        </ul>
+      )}
     </div>
   );
 };
