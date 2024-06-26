7d67a306364eed89fc571b97d9de2efbc5a9de29
Gary February <gfebruary@gmail.com>
2024-06-26 12:36:09 +0200

    sontact section

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Contact.css";
+import linkedinIcon from "./assets/LinkedIn_icon.svg";
+import githubIcon from "./assets/github-icon-2.svg";
 
 const Contact = () => {
   return (
@@ -7,18 +9,15 @@ const Contact = () => {
       Contact
       <p className="section-text"></p>
       <div className="contact-wrapper">
-        <img
-          className="contact-icon"
-          src="../src/assets/LinkedIn_icon.svg.png"
-          alt=""
-          srcset=""
-        />
-        <img
-          className="contact-icon"
-          src="../src/assets/github-icon-2.svg"
-          alt=""
-          srcset=""
-        />
+        <img className="contact-icon" src="{githubIcon}}" alt="" srcset="" />{" "}
+        <span> See more of what I have been working on </span>
+        <img className="contact-icon" src="{linkedinIcon}" alt="" srcset="" />
+        <span>
+          {" "}
+          Feel free to reach out to me if you are looking for a driven and
+          knowledgeable IT professional to join your team or if you simply want
+          to exchange ideas{" "}
+        </span>
       </div>
     </div>
   );
