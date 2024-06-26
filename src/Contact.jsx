60202f74e2e137561fd9bb54b7a723a82ce10700
Gary February <gfebruary@gmail.com>
2024-06-26 13:09:18 +0200

    bug fixes

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -7,17 +7,28 @@ const Contact = () => {
   return (
     <div id="contact" className="section section-heading">
       Contact
-      <p className="section-text"></p>
-      <div className="contact-wrapper">
-        <img className="contact-icon" src={githubIcon} alt="" srcset="" />{" "}
-        <span> See more of what I have been working on </span>
-        <img className="contact-icon" src={linkedinIcon} alt="" srcset="" />
-        <span>
-          {" "}
-          Feel free to reach out to me if you are looking for a driven and
-          knowledgeable IT professional to join your team or if you simply want
-          to exchange ideas{" "}
-        </span>
+      <div className="section-text">
+        <div className="contact-wrapper">
+          <div className="contact-item">
+            <img className="contact-icon" src={githubIcon} alt="GitHub Icon" />
+            <span className="contact-text">
+              See more of what I have been working on
+            </span>
+          </div>
+
+          <div className="contact-item">
+            <img
+              className="contact-icon"
+              src={linkedinIcon}
+              alt="LinkedIn Icon"
+            />
+            <span className="contact-text">
+              Feel free to reach out to me if you are looking for a driven and
+              knowledgeable IT professional to join your team or if you simply
+              want to exchange ideas
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -10,98 +10,95 @@ const Projects = () => {
     <div id="projects" className="section section-heading">
       Projects
       <div className="section-text">
-        <br />
-        <div className="project">
-          <div className="project-content">
-            <a
-              href="https://moviereview-team1.netlify.app/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="project-image-link">
-              <img
-                src={MovieReviewThumb}
-                alt="Movie Review Page"
-                className="project-image"
-              />
-            </a>
-            <div className="project-description">
-              <h3 className="project-title">
-                Social Media Movie Review Page - Group Project
-              </h3>
-              <p className="project-tech">
-                NeonDB, ReactJS, Render, Express, Tailwind, json
-              </p>
+        <div className="project-wrapper">
+          <div className="project">
+            <div className="project-content">
+              <a
+                href="https://moviereview-team1.netlify.app/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="project-image-link">
+                <img
+                  src={MovieReviewThumb}
+                  alt="Movie Review Page"
+                  className="project-image"
+                />
+              </a>
+              <div className="project-description">
+                <h3 className="project-title">
+                  Social Media Movie Review Page - Group Project
+                </h3>
+                <p className="project-tech">
+                  NeonDB, ReactJS, Render, Express, Tailwind, json
+                </p>
+              </div>
             </div>
           </div>
-        </div>
 
-        <br />
-        <div className="project">
-          <div className="project-content">
-            <a
-              href="https://erdinc96.github.io/HackerNews/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="project-image-link">
-              <img
-                src={HackerThumb}
-                alt="Hacker News Page"
-                className="project-image"
-              />
-            </a>
-            <div className="project-description">
-              <h3 className="project-title">
-                Reworking of the HackerNews Website - Group Project
-              </h3>
-              <p className="project-tech">
-                ReactJS, Tailwind, SemanticUI, json
-              </p>
+          <div className="project">
+            <div className="project-content">
+              <a
+                href="https://erdinc96.github.io/HackerNews/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="project-image-link">
+                <img
+                  src={HackerThumb}
+                  alt="Hacker News Page"
+                  className="project-image"
+                />
+              </a>
+              <div className="project-description">
+                <h3 className="project-title">
+                  Reworking of the HackerNews Website - Group Project
+                </h3>
+                <p className="project-tech">
+                  ReactJS, Tailwind, SemanticUI, json
+                </p>
+              </div>
             </div>
           </div>
-        </div>
 
-        <br />
-        <div className="project">
-          <div className="project-content">
-            <a
-              //   href=""
-              target="_blank"
-              rel="noopener noreferrer"
-              className="project-image-link">
-              <img
-                src={Placeholder}
-                alt="IP Project Page"
-                className="project-image"
-              />
-            </a>
-            <div className="project-description">
-              <h3 className="project-title">
-                Using API's to find user location - Solo Project
-              </h3>
-              <p className="project-tech">ReactJS, APIs, Tailwind</p>
+          <div className="project">
+            <div className="project-content">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                className="project-image-link">
+                <img
+                  src={Placeholder}
+                  alt="IP Project Page"
+                  className="project-image"
+                />
+              </a>
+              <div className="project-description">
+                <h3 className="project-title">
+                  Using API's to find user location - Solo Project
+                </h3>
+                <p className="project-tech">ReactJS, APIs, Tailwind</p>
+              </div>
             </div>
           </div>
-        </div>
 
-        <br />
-        <div className="project">
-          <div className="project-content">
-            <a
-              href="https://gfebruary.github.io/to-do-list/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="project-image-link">
-              <img
-                src={ToDoThumb}
-                alt="ToDo List Thumb"
-                className="project-image"
-              />
-            </a>
-            <div className="project-description">
-              <h3 className="project-title">
-                Simple ToDo List - Group Project
-              </h3>
-              <p className="project-tech">ReactJS,Tailwind, LocalStorage</p>
+          <div className="project">
+            <div className="project-content">
+              <a
+                href="https://gfebruary.github.io/to-do-list/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="project-image-link">
+                <img
+                  src={ToDoThumb}
+                  alt="ToDo List Thumb"
+                  className="project-image"
+                />
+              </a>
+              <div className="project-description">
+                <h3 className="project-title">
+                  Simple ToDo List - Group Project
+                </h3>
+                <p className="project-tech">ReactJS,Tailwind, LocalStorage</p>
+              </div>
             </div>
           </div>
         </div>
