68adb36f60cc1add3717f71cef9fead132cf4e97
Gary February <gfebruary@gmail.com>
2024-06-26 17:03:22 +0200

    email update

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./Footer.css";
+import email from "./assets/email.jpg";
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
         <p>&copy; 2024 Gary February</p>
-        <p>Contact: gfebruary@gmail.com</p>
+        <img src={email} alt="email address" srcset="" />
       </div>
     </footer>
   );
